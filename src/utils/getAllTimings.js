ed31adaad34786c865062d5d219529eb402bf255
Tahsin Ahmed <tahsinahmed95@gmail.com>
2021-04-14 15:01:01 -0400

    fixed timer bug and added new favicon

--- src/utils/getAllTimings.js
+++ src/utils/getAllTimings.js
@@ -10,20 +10,24 @@ export function getAllTimings(time) {
 }
 
 function parseMillisecondsIntoReadableTime(milliseconds) {
+	if (milliseconds <= 0) {
+		return { hour: 0, minute: 0, second: 0 };
+	}
+
 	//Get hours from milliseconds
-	var hours = milliseconds / (1000 * 60 * 60);
-	var absoluteHours = Math.floor(hours);
-	var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+	let hours = milliseconds / (1000 * 60 * 60);
+	let absoluteHours = Math.floor(hours);
+	let h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
 
 	//Get remainder from hours and convert to minutes
-	var minutes = (hours - absoluteHours) * 60;
-	var absoluteMinutes = Math.floor(minutes);
-	var m = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes;
+	let minutes = (hours - absoluteHours) * 60;
+	let absoluteMinutes = Math.floor(minutes);
+	let m = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes;
 
 	//Get remainder from minutes and convert to seconds
-	var seconds = (minutes - absoluteMinutes) * 60;
-	var absoluteSeconds = Math.floor(seconds);
-	var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
+	let seconds = (minutes - absoluteMinutes) * 60;
+	let absoluteSeconds = Math.floor(seconds);
+	let s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
 
 	return { hour: h, minute: m, second: s };
 }
